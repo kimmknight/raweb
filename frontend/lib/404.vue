696e4a5dc9c0f32ed7c2b8c57d2ee711a0b366ea
Jack Buehner <git@jackbuehner.com>
2025-09-22 17:46:52 -0400

    feat: add 404 page

---
+++ frontend/lib/404.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+  import { Button, TextBlock } from '$components';
+  import { favoritesEnabled, simpleModeEnabled } from '$utils';
+</script>
+
+<template>
+  <article>
+    <img src="./assets/thinking_face_animated.png" alt="" />
+    <TextBlock variant="subtitle">Page not found</TextBlock>
+    <div class="prose">
+      <TextBlock block>We couldn't find what you are looking for.</TextBlock>
+      <TextBlock block>Error code: 404</TextBlock>
+    </div>
+    <div class="buttons">
+      <div class="button-row">
+        <RouterLink
+          to="/favorites"
+          custom
+          v-slot="{ href, isActive, navigate }"
+          v-if="favoritesEnabled && !simpleModeEnabled"
+        >
+          <Button variant="standard" :href="href" @click="navigate">Go to favorites</Button>
+        </RouterLink>
+        <RouterLink to="/simple" custom v-slot="{ href, isActive, navigate }" v-else-if="simpleModeEnabled">
+          <Button variant="standard" :href="href" @click="navigate">Go to apps and desktops</Button>
+        </RouterLink>
+        <RouterLink to="/apps" custom v-slot="{ href, isActive, navigate }" v-else>
+          <Button variant="standard" :href="href" @click="navigate">Go to apps</Button>
+        </RouterLink>
+        <RouterLink to="/settings" custom v-slot="{ href, isActive, navigate }">
+          <Button variant="standard" :href="href" @click="navigate">Go to settings</Button>
+        </RouterLink>
+      </div>
+    </div>
+  </article>
+</template>
+
+<style scoped>
+  article {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    block-size: 100%;
+    inline-size: 100%;
+    gap: 16px;
+    padding: 24px 16px;
+    background-color: var(--wui-subtle-transparent);
+    border-radius: var(--wui-control-corner-radius);
+    box-sizing: border-box;
+    height: 100%;
+    text-align: center;
+  }
+  article .buttons {
+    text-align: center;
+  }
+  article .prose > * {
+    margin-bottom: 4px;
+  }
+  article .prose > *:last-child {
+    margin-bottom: 2px;
+  }
+  article .button-row {
+    display: flex;
+    flex-direction: row;
+    gap: 8px;
+    margin-bottom: 8px;
+  }
+</style>
