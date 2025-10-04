5bd2bf8d482203a3b5807b4439adf49efee39fed
Jack Buehner <git@jackbuehner.com>
2025-10-04 04:06:10 -0400

    feat: rename Dcoumentation to Wiki

--- frontend/lib/docs-entry.dist.mjs
+++ frontend/lib/docs-entry.dist.mjs
@@ -88,7 +88,7 @@ router.beforeEach((to, from, next) => {
 });
 
 router.afterEach((to) => {
-  document.title = to.meta.title ? `${to.meta.title} - RAWeb Documentation` : 'RAWeb Documentation';
+  document.title = to.meta.title ? `${to.meta.title} - RAWeb Wiki` : 'RAWeb Wiki';
 });
 
 const pinia = createPinia();
