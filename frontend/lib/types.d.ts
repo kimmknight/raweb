91f8e20b7acec440b92c97b51ecbdf10dd98fe89
Jack Buehner <git@jackbuehner.com>
2026-05-16 02:37:07 -0400

    feat(frontend): add custom window events for when the frontend app is ready

--- frontend/lib/types.d.ts
+++ frontend/lib/types.d.ts
@@ -1,7 +1,36 @@
 declare global {
   interface Window {
     __pinia: import('pinia').Pinia;
+
+    addEventListener(
+      event: 'RAWebReady',
+      listener: (this: Window, event: RAWebReadyEvent) => any,
+      options?: boolean | AddEventListenerOptions
+    ): void;
+    removeEventListener(
+      event: 'RAWebReady',
+      listener: (this: Window, event: RAWebReadyEvent) => any,
+      options?: boolean | EventListenerOptions
+    ): void;
+
+    addEventListener(
+      event: 'RAWebAppMounted',
+      listener: (this: Window, event: RAWebAppMountedEvent) => any,
+      options?: boolean | AddEventListenerOptions
+    ): void;
+    removeEventListener(
+      event: 'RAWebAppMounted',
+      listener: (this: Window, event: RAWebAppMountedEvent) => any,
+      options?: boolean | EventListenerOptions
+    ): void;
   }
+
+  interface RAWebReadyEventData {
+    router: import('vue-router').Router;
+  }
+
+  interface RAWebReadyEvent extends CustomEvent<RAWebReadyEventData> {}
+  interface RAWebAppMountedEvent extends CustomEvent<RAWebReadyEventData> {}
 }
 
 export {};
