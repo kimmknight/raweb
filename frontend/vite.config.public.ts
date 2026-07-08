c6127a0e4aba7a6dc7f627db90c743a4fb4c55f8
Jack Buehner <git@jackbuehner.com>
2026-07-08 00:47:02 -0400

    fix: hide %raweb.overrides% text from public build

--- frontend/vite.config.public.ts
+++ frontend/vite.config.public.ts
@@ -89,7 +89,7 @@ export default defineConfig(async ({ command, mode }) => {
               const filePath = `${distDir}/${file}`;
               const html = (await readFile(filePath, 'utf-8'))
                 .replace('%raweb.basetag%', `<base href="${viteConfig.base}" />`)
-                .replace('%usercontent%', '');
+                .replace('%raweb.overrides%', '');
               await writeFile(filePath, html, 'utf-8');
             }
           },
