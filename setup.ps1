2da8614054f0445f6a49d4bc3e4b250a2f50a222
Jack Buehner <git@jackbuehner.com>
2026-07-13 00:12:31 -0400

    fix: remove icon.ico acl edits because icon.ico no longer exists

--- setup.ps1
+++ setup.ps1
@@ -1658,13 +1658,6 @@ Get-ChildItem -Path $dataProtectionKeysPath -Recurse | ForEach-Object {
     Set-Acl -Path $_.FullName -AclObject $dataProtectionKeysAcl
 }
 
-# allow everyone to read the RAWeb icon
-$iconPath = Join-Path $versionedDir "lib\assets\icon.ico"
-$iconAcl = Get-Acl $iconPath
-$everyoneSid = New-Object System.Security.Principal.SecurityIdentifier("S-1-1-0")
-$iconAcl.SetAccessRule((New-Object System.Security.AccessControl.FileSystemAccessRule($everyoneSid, "Read", "None", "None", "Allow")))
-Set-Acl -Path $iconPath -AclObject $iconAcl
-
 # [9] IIS application and authentication ──────────────────────────────────────
 
 Write-Host "[9/13] Configuring IIS application..." -ForegroundColor Cyan
